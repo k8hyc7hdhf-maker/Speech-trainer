d8c392044b2daaf7092d47f2cd4e54630d425617
k8hyc7hdhf-maker <312900895+k8hyc7hdhf-maker@users.noreply.github.com>
2026-08-06 10:13:10 +0900

    2

--- app.js
+++ app.js
@@ -1,12 +1,99 @@
-const createBtn = document.getElementById("createBtn");
+const lesson = [
 
-const setupScreen = document.getElementById("setupScreen");
-const playerScreen = document.getElementById("playerScreen");
+{
 
-createBtn.onclick = () => {
+ru:"Я раньше жил в деревне.",
 
-    setupScreen.classList.add("hidden");
+en:"I used to live in a village."
 
-    playerScreen.classList.remove("hidden");
+},
+
+{
+
+ru:"Я привык вставать рано.",
+
+en:"I'm used to getting up early."
+
+},
+
+{
+
+ru:"Не откладывай это на завтра.",
+
+en:"Don't put it off until tomorrow."
+
+}
+
+];
+
+
+
+const setupScreen=document.getElementById("setupScreen");
+
+const playerScreen=document.getElementById("playerScreen");
+
+const createBtn=document.getElementById("createBtn");
+
+const ruText=document.getElementById("ruText");
+
+const enText=document.getElementById("enText");
+
+
+
+let index=0;
+
+
+
+createBtn.onclick=()=>{
+
+setupScreen.classList.add("hidden");
+
+playerScreen.classList.remove("hidden");
+
+playerScreen.classList.add("active");
+
+startPlayer();
 
 };
+
+
+
+function startPlayer(){
+
+showSentence();
+
+}
+
+
+
+function showSentence(){
+
+const item=lesson[index];
+
+ruText.textContent=item.ru;
+
+enText.textContent=item.en;
+
+enText.style.visibility="hidden";
+
+setTimeout(()=>{
+
+enText.style.visibility="visible";
+
+},5000);
+
+setTimeout(()=>{
+
+index++;
+
+if(index>=lesson.length){
+
+index=0;
+
+}
+
+showSentence();
+
+},7000);
+
+}
